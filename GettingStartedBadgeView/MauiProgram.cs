a65e6691a008406e30fae97da1c6e38f8192fc74
Arunachalam13 <karunachalam406@gmail.com>
2023-09-01 12:59:56 +0530

    Added the sample

---
+++ GettingStartedBadgeView/MauiProgram.cs
@@ -0,0 +1,26 @@
+﻿using Syncfusion.Maui.Core.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace GettingStartedBadgeView;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+            .ConfigureSyncfusionCore()
+            .ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
